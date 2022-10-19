a852edc7e9225cc08ddf6387c98b4a5639ae10ff
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-10-19 00:24:40 -0300

    feat: adicionando servidor receber mensagem

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
-var SRC_PORT = 6025;
-var PORT = 1900;
-var MULTICAST_ADDR = '239.255.255.250';
+const SRC_PORT = 6025;
+const PORT = 1900;
+const MULTICAST_ADDR = '239.255.255.250';
 var dgram = require('dgram');
 var server = dgram.createSocket("udp4");
 
-server.bind(SRC_PORT, function() {
-    setInterval(multicastNew, 4000);
+server.bind(SRC_PORT, () => {
+    setInterval(multicastNew, 7000);
 });
 
-function multicastNew() {
 
-    var object = Buffer.from('SERVER - teste')
-    server.send(object, 0, object.length, PORT, MULTICAST_ADDR, function() {
-        console.log("Teste mensagem '" + object + "'");
+const multicastNew = () => {
+    let message = Buffer.from('ssdp:server')
+    server.send(message, 0, message.length, PORT, MULTICAST_ADDR, () => {
+        console.log("Keep-alive to client: '" + message + "'");
     });
 }
+
+server.on("message", (message, rinfo) => {
+    console.log(`${rinfo.address}:${rinfo.port} - ${message}`)
+})
